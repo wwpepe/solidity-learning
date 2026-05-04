e6afd480b476002ff9bfc54cc5043a747ffc1b06
wwpepe <jhjh1664@naver.com>
2026-05-04 19:39:40 +0900

    refactor update Reward by modifier

--- contracts/TinyBank.sol
+++ contracts/TinyBank.sol
@@ -8,11 +8,19 @@ interface IMyToken {
     function transfer(uint256 amount, address to) external;
 
     function transferFrom(address from, address to, uint256 amount) external;
+
+    function mint(uint256 amount, address owner) external;
 }
 
 contract TinyBank {
-    event Staked(address, uint256);
+    event Staked(address from, uint256 amount);
+    event Withdraw(uint256 amount, address to);
+
     IMyToken public stakingToken;
+    mapping(address => uint256) public lastClaimedBlock;
+
+    uint256 rewardPerBlock = 1 * 10 ** 18;
+
     mapping(address => uint256) public staked;
     uint256 public totalStaked;
 
@@ -20,12 +28,35 @@ contract TinyBank {
         stakingToken = _stakingToken;
     }
 
-    function stake(uint256 _amount) external {
+    //reward
+    modifier updateReward(address to) {
+        if (staked[to] > 0) {
+            uint256 blocks = block.number - lastClaimedBlock[to];
+            uint256 reward = (blocks * rewardPerBlock * staked[to]) /
+                totalStaked;
+            stakingToken.mint(reward, to);
+        }
+        lastClaimedBlock[to] = block.number;
+        _; //caller
+    }
+
+    //예금
+    function stake(uint256 _amount) external updateReward(msg.sender) {
         require(_amount >= 0, "cannot stake 0 amount");
 
         stakingToken.transferFrom(msg.sender, address(this), _amount);
         staked[msg.sender] += _amount;
         totalStaked += _amount;
         emit Staked(msg.sender, _amount);
+    }
+
+    //출금
+    function withdraw(uint256 _amount) external updateReward(msg.sender) {
+        require(staked[msg.sender] >= _amount, "insufficient staked token");
+
+        stakingToken.transfer(_amount, msg.sender);
+        staked[msg.sender] -= _amount;
+        totalStaked -= _amount;
+        emit Withdraw(_amount, msg.sender);
     }
 }
