235e8dc877b5fe0264c6ba87a07fe482ee34bc3e
wwpepe <jhjh1664@naver.com>
2026-05-05 20:05:03 +0900

    access manager apply to TinyBank

--- test/MyToken.ts
+++ test/MyToken.ts
@@ -54,7 +54,7 @@ describe("My token", () => {
       const mintingAgainAmount = hre.ethers.parseUnits("100", DECIMALS);
       await expect(
         myTokenC.connect(hacker).mint(mintingAgainAmount, hacker.address),
-      ).to.be.revertedWith("You are not authorized to manage this token");
+      ).to.be.revertedWith("You are not authorized to manage this contract");
     });
   });
 
